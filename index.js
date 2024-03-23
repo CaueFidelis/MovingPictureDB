144b435f11e6241f69ce6816f1c2c7b8f557fcf6
cauefidelis <cauefidelis@live.com>
2024-03-23 17:13:51 -0300

    Uncommenting index codes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const imdbApi = require("./lib/imdb-node-api");
 
 // Movie Details of a specific movie by ID
-// imdbApi
-//   .getMovieDetails(949697)
-//   .then((data) => {
-//     console.log(data);
-//   })
-//   .catch((error) => {
-//     console.log("erro", error);
-//   });
+imdbApi
+  .getMovieDetails(572802)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("erro", error);
+  });
 
 // Movies Per Year - 20 movies per page
 imdbApi
@@ -20,12 +20,12 @@ imdbApi
     console.error(error);
   });
 
-// Top Rated
-// imdbApi
-//   .topRatedMovies({ page: 1 })
-//   .then((result) => {
-//     console.log(result);
-//   })
-//   .catch((error) => {
-//     console.error(error);
-//   });
+// Top Rated Movies - 20 per page
+imdbApi
+  .topRatedMovies({ page: 1 })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
