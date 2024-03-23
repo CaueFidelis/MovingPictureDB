e36532d5f822fe0b909f29457e7d4b968e8894a0
cauefidelis <cauefidelis@live.com>
2024-03-23 17:00:02 -0300

    Configuring the standards functions of IMDB API consumption

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-module.exports = require('./lib/imdb-node-api');
+const imdbApi = require("./lib/imdb-node-api");
+
+// Movie Details
+imdbApi
+  .getMovieDetails(949697)
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log("erro", error);
+  });
+
+// Per Year
+imdbApi
+  .searchMoviesPerYear({ page: 1, year: 2022 })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Top Rated
+imdbApi
+  .topRatedMovies({ page: 1 })
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
