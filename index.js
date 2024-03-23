08c1ff9674104ccf86ad8bf5bb16542d904f1a3d
cauefidelis <cauefidelis@live.com>
2024-03-23 17:08:09 -0300

    fixing writing error

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const imdbApi = require("./lib/imdb-node-api");
 
-// Movie Details
-imdbApi
-  .getMovieDetails(949697)
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => {
-    console.log("erro", error);
-  });
+// Movie Details of a specific movie by ID
+// imdbApi
+//   .getMovieDetails(949697)
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => {
+//     console.log("erro", error);
+//   });
 
-// Per Year
+// Movies Per Year - 20 movies per page
 imdbApi
-  .searchMoviesPerYear({ page: 1, year: 2022 })
+  .searchMoviesPerYear({ page: 1, year: 2023 })
   .then((result) => {
     console.log(result);
   })
@@ -21,11 +21,11 @@ imdbApi
   });
 
 // Top Rated
-imdbApi
-  .topRatedMovies({ page: 1 })
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// imdbApi
+//   .topRatedMovies({ page: 1 })
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
